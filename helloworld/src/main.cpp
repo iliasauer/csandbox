3c7dabc7560f0911f014293b1ffcd10f55d1bd6b
Ilia Kisliakovskii <kisliakovskiii@yandex.ru>
2016-07-10 08:38:55 +0300

    multiprojects2

---
+++ helloworld/src/main.cpp
@@ -0,0 +1,16 @@
+//
+// Created on 7/10/2016.
+//
+#include <iostream>
+#include "math.h"
+
+using namespace std;
+
+int main() {
+    char name[] = "";
+    cout << "Enter your name: ";
+    cin >> name;
+    cout << "Hello, " << name << '!' << endl;
+    return 0;
+};
+
