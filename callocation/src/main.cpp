56cd40c8388dc731727310c5ecb86ff6c3495dad
Ilia Kisliakovskii <kisliakovskiii@yandex.ru>
2016-07-13 19:36:48 +0300

    callocation

---
+++ callocation/src/main.cpp
@@ -0,0 +1,17 @@
+//
+// Created on 7/10/2016.
+//
+#include <cstdlib>
+
+using namespace std;
+
+int main() {
+    size_t intSize = sizeof(int);
+    int * array = (int *) malloc(1000 * intSize);
+    array = (int *) realloc(array, 2000 * intSize);
+    free(array);
+    array = (int *) calloc(3000, intSize);
+    free(array);
+    array = 0;
+};
+
