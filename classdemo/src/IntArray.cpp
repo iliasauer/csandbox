e8d9b2624db46141b6190f5e033c3b392aaef392
Ilya Kisliakovsky <ilya.kisliakovsky@emc.com>
2016-07-20 18:47:39 +0300

    int array 2

--- classdemo/src/IntArray.cpp
+++ classdemo/src/IntArray.cpp
@@ -1,7 +1,7 @@
 //
 // Created by ilya on 20.07.16.
 //
-
+#include <algorithm>
 #include "IntArray.h"
 
 IntArray::IntArray(int const size) : size_(size), data_(new int[size]) {
@@ -15,6 +15,60 @@ IntArray::IntArray(IntArray const &other) : size_(other.size_), data_(new int[ot
         data_[i] = other.data_[i];
     }
 }
+
+IntArray::~IntArray() {
+    delete [] data_;
+}
+
+IntArray &IntArray::operator=(IntArray const &other) {
+    if (this != &other) {
+        IntArray(other).swap(*this);
+    }
+    return *this;
+}
+
+void IntArray::swap(IntArray &other) {
+    std::swap(size_, other.size_);
+    std::swap(data_, other.data_);
+}
+
+int IntArray::size() const {
+    return size_;
+}
+
+int IntArray::get(const int i) const {
+    return data_[i];
+}
+
+int &IntArray::get(const int i) {
+    return data_[i];
+}
+
+void IntArray::resize(int nsize) {
+    IntArray newArray(nsize);
+    int n;
+    if (size_ < nsize) {
+        n = size_;
+    } else {
+        n = nsize;
+    }
+    for (int i = 0; i < n; i++) {
+        newArray.data_[i] = data_[i];
+    }
+    swap(newArray);
+}
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
--- classdemo/src/IntArray.h
+++ classdemo/src/IntArray.h
@@ -12,9 +12,20 @@ class IntArray {
 private:
     int size_;
     int *  data_;
+
+    virtual void swap(IntArray &other);
 public:
     explicit IntArray(int const size);
     IntArray(IntArray const& other);
+
+    virtual IntArray &operator=(IntArray const &other);
+
+    virtual ~IntArray();
+
+    virtual int size() const;
+    virtual int get(int i const) const;
+    virtual int & get(int i const);
+    virtual void resize(int nsize);
 };
 
 
