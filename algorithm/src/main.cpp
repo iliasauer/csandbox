6de118f744f2c1455c73e02c32357bb6880bd80c
Ilia Kisliakovskii <kisliakovskiii@yandex.ru>
2016-07-23 22:47:56 +0300

    std algorithms

--- algorithm/src/main.cpp
+++ algorithm/src/main.cpp
@@ -2,15 +2,19 @@
 // Created on 7/10/2016.
 //
 #include <iostream>
+#include <vector>
 #include <algorithm>
 
-using namespace std;
 
 int main() {
-    vector<int> vector = {1, 100, 43, 0, -12, 82, 45, 3};
-    sort(vector.begin(), vector.end());
-    for (auto x: vector) {
-        std::cout << x << ' ';
+    std::vector<int> vector = {-8, 42, -78, -30, 32, 5, 11, 0};
+    std::vector<std::pair<int, int>> pairVector;
+    for (int i = 0; i < vector.size(); ++i) {
+        pairVector.push_back({vector[i], i});
+    }
+    sort(pairVector.begin(), pairVector.end());
+    for (auto x: pairVector) {
+        std::cout << x.second << ' ';
     }
     std::cout << std::endl;
     return 0;
